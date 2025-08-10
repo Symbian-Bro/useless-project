9800c98cffca50136c927317bd019d33f324f270
Symbian-Bro <siddhubhai998@gmail.com>
2025-08-10 11:26:22 +0530

    Fixed a typo

--- Main Sketch/Main.ino
+++ Main Sketch/Main.ino
@@ -1,5 +1,5 @@
 int ldrpin = A3; //Code fails to compile when I remove this line. I have no idea why.
-// Only A3 used inside the main code snippets
+// Only A3 is used inside the main code snippets
 void setup(){
   pinMode(A3,INPUT);
   Serial.begin(9600);
