d1778145e573c964aa1df44c976569f0f29c0bc3
Symbian-Bro <siddhubhai998@gmail.com>
2025-08-10 14:44:23 +0530

    The Serial_Reader function has been modified. Now the function will start working after a sleep time of 5 minutes. The first that the function will do after waking up is to record the value at that instant of time as the thresold. Then values that the script receives later is compared to this threshold to regulate the printing speed. +-100 is an example to demonstrate the logic. The constraints for the fluctuation must be derived from actual physical environments instead of hardcoding it like this :)

--- Python files/Functions/Serial Reader.py
+++ Python files/Functions/Serial Reader.py
@@ -1,5 +1,8 @@
 import serial
 import time
+
+delay_minutes = 5 #Time after which the threshold will be set (Refer to Logic)
+time.sleep(delay_minutes * 60)
 def serial_reader(port,bitrate=9600,timeout=1):
     data = serial.Serial(port, bitrate, timeout=timeout)
     time.sleep(2)
@@ -11,6 +14,16 @@ def serial_reader(port,bitrate=9600,timeout=1):
                 return line
             except ValueError:
                 continue
-while True:
+
+threshold = serial_reader('/dev/ttyUSB0', 9600)
+
+while (True):
     y = serial_reader('/dev/ttyUSB0', 9600)
-    print(y)
+    if y > (threshold+100):
+        #Slow down
+    elif y < (threshold-100):
+        #Speed up
+    else:
+        #I don't know what to put here
+
+
