e5c4d657d49574f555cfb83cb830387627f33679
Symbian-Bro <siddhubhai998@gmail.com>
2025-08-19 20:08:06 +0530

    Added the speed_regulation.py file into the main file

--- Python files/main.py
+++ Python files/main.py
@@ -93,3 +93,17 @@ while (True):
         flag = 0
     else:
         pass
+
+if (flag==1):
+    n = word_counter()
+    if (n>5):
+        print("Word count exceeded 5, please wait 60 seconds to type more.")
+        capture_keypress(event)
+        time.sleep(60)
+elif (flag==0):
+    if (n>12):
+        print("Word count exceeded 12, please wait 60 seconds to type more.")
+        capture_keypress(event)
+        time.sleep(60)
+else:
+    pass
