6b017b63f91b4a5dabce3945c80e2514eebd9105
Symbian-Bro <siddhubhai998@gmail.com>
2025-08-19 17:49:22 +0530

    Integrated the serial_reader function with the main file.

--- Python files/main.py
+++ Python files/main.py
@@ -14,3 +14,28 @@ def port_finder():
 
 data = serial.Serial(port_id, 9600, timeout=timeout)
 time.sleep(2)
+
+def serial_reader(data,timeout=1):
+    while(True):
+        if data.in_waiting > 0:
+            try:
+                line = int(data.readline().decode('utf-8').strip())
+                return line
+            except ValueError:
+                continue
+
+current_time = time.time()
+value = []
+while (time.time() - current_time < 60):
+    value.append(serial_reader(data))
+
+threshold = int(sum(value) / len(value))
+
+while (True):
+    current_value = serial_reader(data)
+    if current_value > (threshold+100):
+        flag = 1
+    elif current_value < (threshold-100):
+        flag = 0
+    else:
+        pass
